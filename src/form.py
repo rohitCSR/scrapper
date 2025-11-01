73092f9eebe87972708a8d55aa8c7efbd9d07aa4
rohitCSR <rohitbm243@gmail.com>
2025-11-01 20:50:45 +0530

    Add streamlit config and improve job portals data.

--- src/form.py
+++ src/form.py
@@ -1,11 +1,17 @@
+import json
+
 import streamlit as st
+
+SITES_DATA = json.load(open("./src/job_sites.json", "r"))
+SITES = [site["site"] for site in SITES_DATA]
 
 
 def search_form():
     with st.container(horizontal=True, vertical_alignment="bottom"):
         position = st.text_input("Position")
         site = st.selectbox(
-            "Choose site", options=["greenhouse.com", "jobs.lever.co", "glassdoor.com"]
+            "Choose site",
+            options=SITES,
         )
         location = st.selectbox(
             "Choose location",
